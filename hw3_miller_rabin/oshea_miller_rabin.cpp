499572f34c2f2a77600b0b95db82a32253085e30
Ryan O'Shea <rpo1202@gmail.com>
2019-09-14 17:26:01 -0400

    Added file for hw 3 Miller Rabin implementation. Need to fix random number generation.

---
+++ hw3_miller_rabin/oshea_miller_rabin.cpp
@@ -0,0 +1,138 @@
+/*
+	 Author: Ryan O'Shea
+
+	 Description: Takes in integer through the command line and displays whether
+	 it is a prime or a composite number
+
+	 cite: http://www.cplusplus.com/reference/cstdlib/rand/ for random numbers
+	 https://stackoverflow.com/questions/22746429/c-decimal-to-binary-converting
+	 	converting int to binary
+	 http://www.cplusplus.com/reference/string/string/pop_back/ for string element pop
+
+	 "I pledge my honor that I have abided by the Stevens Honor System"
+*/
+
+#include <iostream>
+#include <stdlib.h>     /* srand, rand */
+#include <bitset>
+#include <string>
+
+using namespace std;
+
+// Raise a to the power of b and then return the result of (a^b) mod r
+int powermod(int a, int b, int r)
+{
+	int prod = 1;
+
+	while(b > 0)
+	{
+		//If the power is Odd
+		if(b%2 != 0)
+		{
+			prod = (prod * a) % r; // Multiply the product by a so we now have an even power then mod it by r
+		}
+
+		a = (a*a) % r; // square a then mod it by r
+		b /= 2; // Since we've square a we can divide the exponent it needs to be
+		// raised to by 2
+	}
+	return prod;
+}
+
+// Returns whether a number is not prime or most likely prime
+bool millerRabin(int num, int trials)
+{
+	// Loop for number of trials
+	for(int i = 1; i <= trials; i ++)
+	{
+		// Generate random witness number between 2 and num - 2
+		// TODO: Fix random number generation
+		int a = 5; //(rand() % (num - 4)) + 2;
+		cout << "a = " << a << "\n";
+
+		int s = 0;
+		int d;
+
+		// turn n-1 into binary and then split it into s and d based on its trailing 0s
+		string binary = bitset<32>(num-1).to_string();
+		cout << binary << "\n";
+		for(int j = binary.length(); j > 0; j--)
+		{
+			// If we hit a non zero then we are out of trailing 0s
+			if(binary[j-1] != '0')
+			{
+				break; //Break out of loop
+			}
+
+			// Otherwise increment s because we have another trailing 0
+			else
+			{
+				s++;
+				binary.pop_back(); // Remove the last element in the string
+			}
+		}
+
+		cout << "s = " << s << "\n";
+		cout << "cut binary = " << binary << "\n";
+
+		// Store the result in d
+		d = bitset<32>(binary).to_ulong();
+
+		cout << "d = " << d << "\n";
+
+		// Use powermod to see if the number is most likely prime
+		int x = powermod(a, d, num);
+
+		// Check if result from powermod is good
+		if(x == 1 || x == num -1)
+		{
+			// Test passed continue through trials
+			continue;
+		}
+
+		// Bad result need to manually check
+		for(int j = 1; j < s -1; j++)
+		{
+			x = x*x % num;
+
+			// Carmichael number found return false
+			if(x == 1)
+			{
+				return false;
+			}
+
+			// Number checks out continue with trials
+			if(x == num -1)
+			{
+				continue;
+			}
+
+			return false;
+		}
+	}
+	// All trials passed the number is most likely prime
+	return true;
+}
+
+int main(int argc, char *argv[])
+{
+	// Set seed for random numbers
+	srand(0);
+
+	// Get the number from the command line
+	int num_to_check = atoi(argv[1]);
+
+	cout << num_to_check << "\n";
+
+	bool check = millerRabin(num_to_check, 1);
+
+	if(check)
+	{
+		cout << num_to_check << " is most likely prime. \n";
+	}
+
+	else
+	{
+		cout << num_to_check << " is composite. \n";
+	}
+}
