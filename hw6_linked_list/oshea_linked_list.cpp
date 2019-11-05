e49aeadcd069d576352ea3bd20fad30b6008111c
Ryan O'Shea <rpo1202@gmail.com>
2019-11-05 02:01:12 -0500

    Homework 6 Linkedlist assignment. Working linkedlist portion. Insert might need to be slighly modified based on Kruger's definition of it.

---
+++ hw6_linked_list/oshea_linked_list.cpp
@@ -0,0 +1,158 @@
+/*
+	 Author: Ryan O'Shea
+
+	 Description: Takes in the list from a file and then splits it into two seperate
+	 linked lists where one is numbers and one is odd numbers
+
+	 cite:
+
+	 "I pledge my honor that I have abided by the Stevens Honor System"
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class LinkedList{
+private:
+	class Node { //Node object to make the linked list out of
+	public:
+		int val;
+		Node* next; // Points to the next Node object in the list or nullptr
+
+		// Constructor
+		Node(int v, Node* n) : val(v), next(n) {}
+	};
+
+	// Pointer to first node in the list
+	Node* head;
+
+public:
+	// Constructor
+	LinkedList()
+	{
+		head = nullptr;
+	}
+
+	// Add Node to start of linked list.
+	// Head -> New Node -> whatever head was pointing to
+	void addStart(int v)
+	{
+		head = new Node(v, head);
+	}
+
+	// Add Node to the end of the linked list
+	// Head -> middle stuff -> New Node -> nullptr
+	void addEnd(int v)
+	{
+		Node* temp = new Node(v, nullptr); // Node being placed at the end
+		Node* n;
+
+		// Check if there are no nodes in the list
+		if(head == nullptr)
+		{
+			head = temp;
+			return;
+		}
+
+		// Loop until the end of the list is found
+		// Once n->next == nullptr the last node in the list has been found
+		for(n = head; n->next != nullptr; n = n->next)
+		{
+			// Do nothing
+		}
+
+		// n now points to the last node in the list so switch the node that points
+		// to from nullptr to the node we want to add to the end
+		n->next = temp;
+	}
+
+	// Inserts a node at a specified index i
+	// Head -> middle stuff -> node at desired index -> new node -> whatever node at desired index at desired index was pointing to
+	void insert(int i, int v)
+	{
+		Node* n = head;
+
+		// Move the pointer until we've reached the desired node
+		while(i > 0)
+		{
+			n = n->next;
+			i--;
+		}
+
+		// We're at the desired node so insert the new one between the two
+		n->next = new Node(v, n->next);
+	}
+
+	// Remove the node at the beginning of the list and return its value
+	// head -> old node -> what old node was pointing to
+	// becomes
+	// head -> what old node was pointing to
+	int removeStart()
+	{
+		Node* n = head; // Point to whatever head is pointing at to save the node temporarily
+		head = head->next; // Remove the node from the list
+		return n->val; // Return the value from the removed node
+	}
+
+	// Remove the node at the end of the list
+	// head -> middle stuff -> last node -> nullptr
+	// becomes
+	// head -> middle stuff -> nullptr
+	int removeEnd()
+	{
+		Node* n = head;
+		Node* m = n->next;
+
+		// There's only one node in the list
+		if(m == nullptr)
+		{
+			int v = head->val; // Get the value
+			head = nullptr; // Remove the node
+			return v;
+		}
+
+		// Loop until the end of the list is found by m.
+		// Once m->next == nullptr, n will point to the 2nd to last node
+		for(; m->next != nullptr;)
+		{
+			n = m; // Move n to m
+			m = m->next; // Move up m to the next node
+		}
+
+		// n now points to the 2nd to last node and m points to the last node
+		n->next = nullptr; // Remove the last node
+		return m->val;
+
+	}
+
+	// Display all the numbers in the LinkedList
+	void display()
+	{
+		Node* n = head;
+
+		for(; n != nullptr; n = n->next)
+		{
+			cout << n->val << " ";
+
+		}
+	}
+
+};
+
+int main(int argc, char **argv)
+{
+	LinkedList LL;
+
+	LL.addStart(7);
+	LL.addEnd(9);
+	LL.addStart(1);
+	LL.insert(1, 34);
+	LL.removeEnd();
+	LL.removeStart();
+	LL.insert(0, 55);
+	LL.insert(2, 910);
+	LL.display();
+}
